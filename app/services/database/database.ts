638e10d9996b4b47325ecfe5253f272f11197663
Tuomas Salmi <salmi.tuomas@gmail.com>
2022-04-24 16:44:17 +0300

    Set proper mongodb connection options for production env

--- app/services/database/database.ts
+++ app/services/database/database.ts
@@ -2,19 +2,32 @@ import mongoose from 'mongoose';
 import { isDev } from '../../helpers/helpers';
 
 export function getDbConnection() {
-  // Connect to MongoDB.
-  return mongoose.connect(
+  const connectionString =
     `${isDev() ? 'mongodb://' : 'mongodb+srv://'}` +
-      process.env.KILOMETRIKISA_DBUSER +
-      ':' +
-      process.env.KILOMETRIKISA_DBPASSWORD +
-      '@' +
-      process.env.KILOMETRIKISA_DBHOST +
-      '/' +
-      process.env.KILOMETRIKISA_DB +
-      '?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true, authSource: isDev() ? 'admin' : undefined },
-  );
+    process.env.KILOMETRIKISA_DBUSER +
+    ':' +
+    process.env.KILOMETRIKISA_DBPASSWORD +
+    '@' +
+    process.env.KILOMETRIKISA_DBHOST +
+    '/' +
+    process.env.KILOMETRIKISA_DB +
+    '?retryWrites=true&w=majority';
+
+  // Connect to MongoDB.
+  const options: {
+    useNewUrlParser: boolean;
+    useUnifiedTopology: boolean;
+    authSource?: string;
+  } = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  if (isDev()) {
+    options.authSource = 'admin';
+  }
+
+  return mongoose.connect(connectionString, options);
 }
 
 export function disconnectDb() {

d310763f4bfe2a5c20df0b0dbd33828c7273e436
Tuomas Salmi <salmi.tuomas@gmail.com>
2022-04-25 23:03:40 +0300

    Remove unused controller
    The original app-routing setup in index.ts is a bit out there so it took
    a while to realize this is not even used at the moment! Will have to
    refactor that later.

--- app/controllers/Home.ts
+++ app/controllers/Home.ts
@@ -1,14 +1,6 @@
 import { Request, Response } from 'express';
 
 export default {
-  Home: function (req: Request, res: Response) {
-    if (req.session.stravaUserId) {
-      res.redirect('/account');
-    } else {
-      res.render('index', {});
-    }
-  },
-
   faq: function (req: Request, res: Response) {
     //need to put these behind http auth or something
     res.render('faq', {});

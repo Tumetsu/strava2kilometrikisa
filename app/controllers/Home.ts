cb656e3dcebde871d685988eb37bb59bd16f8d16
hannupekka <hannupekka@gmail.com>
2022-04-28 20:38:12 +0300

    Add "Delete my data"-button

--- app/controllers/Home.ts
+++ app/controllers/Home.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
+import { findUser } from '../models/UserModel';
 
 export default {
   faq: function (req: Request, res: Response) {
     //need to put these behind http auth or something
     res.render('faq', {});
   },
-
+  delete: async function (req: Request, res: Response) {
+    try {
+      const user = await findUser({ stravaUserId: req.session.stravaUserId as number });
+      await user.delete();
+      this.logout(req, res);
+    } catch {
+      res.redirect('/?error=usernotfound');
+    }
+  },
   logout: function (req: Request, res: Response) {
     req.session.stravaUserId = null;
     req.session.stravaToken = null;
